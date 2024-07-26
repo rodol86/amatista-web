5088940b4a8166d0e3bdb89e164f6cd3d36b4030
rzavala <rzavala@bpba.com.ar>
2024-07-26 10:11:35 -0300

    nuevo layout para las notas

--- src/components/NotasList.js
+++ src/components/NotasList.js
@@ -22,31 +22,32 @@ const NotaCard = ({ nota }) => {
 
   return (
     <div className="relative bg-white p-4 border border-violet-300 rounded-lg shadow-md">
-      {nota.conFechaVencimiento && (
-        <div className="absolute top-2 right-2 border border-violet-500 rounded p-1">
-          <p className="text-violet-700 text-sm font-bold">
-            {new Date(nota.fechaVencimiento).toLocaleDateString()} {new Date(nota.fechaVencimiento).toLocaleTimeString()}
-          </p>
-        </div>
-      )}
-      <h2 className="text-violet-700 text-lg font-bold mb-2">Nota {nota.id}</h2>
+      <div className="flex justify-between mb-2">
+        {nota.conRecordatorio && (
+          <div className="text-gray-700">
+            <button
+              className="text-violet-500 hover:text-violet-700 focus:outline-none"
+              data-tooltip-id={`tooltip-${nota.id}`}
+              data-tooltip-content={`Tiene recordatorio ${nota.frecuenciaRecordatorio} los días ${nota.diaRecordatorio} a las ${nota.horaRecordatorio}`}
+            >
+              <FontAwesomeIcon icon={faBell} />
+            </button>
+            <Tooltip id={`tooltip-${nota.id}`} place="top" effect="solid" className="z-10" />
+          </div>
+        )}
+      </div>
       <p
         className={`text-gray-700 mb-2 ${expanded ? '' : 'line-clamp-3'}`}
-        style={{ whiteSpace: 'pre-wrap' }}
+        style={{ whiteSpace: 'pre-wrap', cursor: 'pointer' }}
         onClick={toggleExpanded}
       >
         {nota.texto}
       </p>
-      {nota.conRecordatorio && (
-        <div className="text-gray-700 mb-2">
-          <button
-            className="text-violet-500 hover:text-violet-700 focus:outline-none"
-            data-tooltip-id={`tooltip-${nota.id}`}
-            data-tooltip-content={`Tiene recordatorio ${nota.frecuenciaRecordatorio} los días ${nota.diaRecordatorio} a las ${nota.horaRecordatorio}`}
-          >
-            <FontAwesomeIcon icon={faBell} />
-          </button>
-          <Tooltip id={`tooltip-${nota.id}`} place="top" effect="solid" className="z-10" />
+      {nota.conFechaVencimiento && (
+        <div className="flex justify-end mt-4">
+          <div className="border border-violet-500 rounded p-1 text-violet-700 text-sm font-bold">
+            {new Date(nota.fechaVencimiento).toLocaleDateString()} {new Date(nota.fechaVencimiento).toLocaleTimeString()}
+          </div>
         </div>
       )}
     </div>
